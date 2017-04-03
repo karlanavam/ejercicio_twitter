dd1c8b8fac71649c7c61a6f9df9240df9f043b93
karla nava <karla.nm8@gmail.com>
2017-04-03 21:31:18 +0000

    Sirve el contador de texto y publica tweet

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,42 @@
+
+/*var contadorClick = 0;*/
+
 var cajaDeTexto = document.getElementById("cajaTexto");
+var autorTweet = document.getElementById("autorDeTweet");
 var contador = 0;
-var contadorClick = 0;
 
 function contarCaracteres() {
 	contador ++;
 	console.log(contador);
 	document.getElementById("contadorfinal").innerHTML = contador;
+	
 }
 
-function click() {
+/*function click() {
 	contadorClick ++;
 	console.log(contadorClick);
 	document.getElementById("contenedorClicks").innerHTML = contadorClick;
+}*/
+
+function publicarTweet () {
+	var contenedorDeTweets = document.getElementById("tweetsPublicados");
+
+	var tweets = document.createElement("p");
+	var autor = document.createElement("p");
+	
+	tweets.innerText = cajaDeTexto.value;
+	cajaDeTexto = "";
+
+	
+
+	autor.innerText = autorTweet.value;
+	autorTweet = "";
+
+	contenedorDeTweets.appendChild(tweets);
+	contenedorDeTweets.appendChild(autor);
+
 }
+
 
 /*ciclo:
 evento en variable
